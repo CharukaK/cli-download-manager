4dec560e5db5a36cd48d5462b3f0ee6dce75e4f9
CharukaK <charukakarunanayake@gmail.com>
2022-07-19 20:22:40 +0530

    Initiate java project

---
+++ src/main/java/com/download/manager/launcher/App.java
@@ -0,0 +1,13 @@
+package com.download.manager.launcher;
+
+
+import java.util.Arrays;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        Arrays.stream(args).forEach(System.out::println);
+    }
+}
