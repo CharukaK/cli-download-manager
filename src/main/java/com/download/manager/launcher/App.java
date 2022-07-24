0e5bbb63a73a38fe9b157f9a663fed73c6a9a836
CharukaK <charukakarunanayake@gmail.com>
2022-07-24 14:04:26 +0530

    Remove commented out code segments

--- src/main/java/com/download/manager/launcher/App.java
+++ src/main/java/com/download/manager/launcher/App.java
@@ -5,7 +5,6 @@ import com.download.manager.download.DownloadFactory;
 import com.download.manager.download.DownloadManager;
 import com.download.manager.download.GlobalConfig;
 import com.download.manager.exceptions.DownloadException;
-import com.download.manager.util.Constants;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -40,31 +39,4 @@ public class App {
             }
         });
     }
-
-//    private static void extractConfigFromArguments(String[] args, GlobalConfig config, List<List<String>> downloadList)
-//            throws DownloadException {
-//
-//        int index = -1;
-//        for (String arg : args) {
-//            if (arg.startsWith(Constants.RETRY_COUNT_ARGUMENT_OPTION)) {
-//                config.setRetryCount(Integer.parseInt(getOptionValue(arg)));
-//            } else if (arg.startsWith(Constants.RETRY_INTERVAL_ARGUMENT_OPTION)) {
-//                config.setRetryInterval(Integer.parseInt(getOptionValue(arg)));
-//            } else if (arg.startsWith(Constants.OUTPUT_DIR_ARGUMENT_OPTION)) {
-//                config.setDownloadDir(getOptionValue(arg));
-//            } else if (arg.equals(Constants.DOWNLOAD_LIST_DELIMITER)) {
-//                index++;
-//                downloadList.add(new ArrayList<>());
-//            } else {
-//                downloadList.get(index).add(arg);
-//            }
-//        }
-//    }
-//
-//    private static String getOptionValue(String arg) throws DownloadException {
-//        if (!arg.contains("=") && arg.split("=").length == 2) {
-//            throw new DownloadException("Invalid argument format");
-//        }
-//        return arg.split("=")[1];
-//    }
 }
